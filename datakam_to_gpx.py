1ce25e3c650c440e8c0a94b8a4efd8d9c0281de3
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2015-12-18 17:42:10 +0300

    use arguments as file name

--- datakam_to_gpx.py
+++ datakam_to_gpx.py
@@ -2,36 +2,40 @@
 from datetime import datetime
 import gpxpy
 import gpxpy.gpx
+import sys
 
-log_file = '2015-09-03.log'
-gpx_file = log_file + '.gpx'
+def convert_to_gpx(log_file):
+    gpx_file = log_file + '.gpx'
+    with open(log_file) as f:
+        content = f.read().splitlines()
 
-with open(log_file) as f:
-    content = f.read().splitlines()
+    for i, var in enumerate(content):
+        content[i] = var.split('\t')
 
-for i, var in enumerate(content):
-    content[i] = var.split('\t')
+    gpx = gpxpy.gpx.GPX()
 
-gpx = gpxpy.gpx.GPX()
+    gpx_track = gpxpy.gpx.GPXTrack()
+    gpx.tracks.append(gpx_track)
 
-gpx_track = gpxpy.gpx.GPXTrack()
-gpx.tracks.append(gpx_track)
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
 
-gpx_segment = gpxpy.gpx.GPXTrackSegment()
-gpx_track.segments.append(gpx_segment)
+    # name.text = "datakam track " + filename
 
-# name.text = "datakam track " + filename
+    for i, v in enumerate(content):
+        if len(v) > 1:
+            lat = v[1][1:]
+            lon = v[2][1:]
+            elev = v[3]
+            t = datetime.strptime(v[0], "%Y-%m-%d %H:%M:%S")
+            gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=elev, time=t))
 
-for i, v in enumerate(content):
-    if len(v) > 1:
-        lat = v[1][1:]
-        lon = v[2][1:]
-        elev = v[3]
-        t = datetime.strptime(v[0], "%Y-%m-%d %H:%M:%S")
-        gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=elev, time=t))
+    result = gpx.to_xml()
 
-result = gpx.to_xml()
+    result_file = open(gpx_file, 'w')
+    result_file.write(result)
+    result_file.close()
 
-result_file = open(gpx_file, 'w')
-result_file.write(result)
-result_file.close()
+for i, v in sys.argv:
+    if i>1:
+        convert_to_gpx(v)
