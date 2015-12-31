2a6601bbf76b299c34c292e32ad80eabb4f9d62a
Artem Soldatkin <a.soldatkin@corp.badoo.com>
2015-12-31 12:44:31 +0300

    if two points have more than 10 minutes in difference create a new track segment

--- datakam_to_gpx.py
+++ datakam_to_gpx.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
 from datetime import datetime
-import os.path
-import gpxpy
-import gpxpy.gpx
+import os
 import sys
 
+import gpxpy
+import gpxpy.gpx
 
-def convert_to_gpx(log_file):
+
+def get_date_time(datakam_date):
+    """
+
+    :rtype : datetime
+    """
+    result = datetime.strptime(datakam_date, '%Y-%m-%d %H:%M:%S')
+    return result
+
+
+def convert_to_gpx(log_file, force=False):
     print log_file + ' converting ...'
     gpx_file = log_file + '.gpx'
-    if os.path.isfile(gpx_file):
+    if force is False and os.path.isfile(gpx_file):
         print gpx_file + ' file exist, skip converting'
         return
 
@@ -21,24 +31,37 @@ def convert_to_gpx(log_file):
 
     gpx = gpxpy.gpx.GPX()
 
+    gpx_track_file_name = os.path.basename(log_file)
     gpx_track = gpxpy.gpx.GPXTrack()
-    gpx_track.name = log_file
+    gpx_track.name = gpx_track_file_name
     gpx.tracks.append(gpx_track)
 
     gpx_segment = gpxpy.gpx.GPXTrackSegment()
     gpx_track.segments.append(gpx_segment)
 
+    last_point_date_time = get_date_time('1970-01-01 00:00:00')
+
     for i, v in enumerate(content):
-        if len(v) > 1:
-            lat = v[1][1:]
-            lon = v[2][1:]
-            elevation = v[3]
-            date_time = datetime.strptime(v[0], "%Y-%m-%d %H:%M:%S")
-            current_speed = v[4]
-            magnetic_variation = v[5]
-            point = gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=elevation, time=date_time, speed=current_speed)
-            point.magnetic_variation = magnetic_variation
-            gpx_segment.points.append(point)
+        if len(v) < 5:
+            continue
+
+        point_date_time = get_date_time(v[0])
+        delta = point_date_time - last_point_date_time
+        if i != 0 and delta.seconds > 60 * 10:
+            gpx_segment = gpxpy.gpx.GPXTrackSegment()
+            gpx_track.segments.append(gpx_segment)
+
+        last_point_date_time = point_date_time
+
+        lat = v[1][1:]
+        lon = v[2][1:]
+        elevation = v[3]
+
+        current_speed = v[4]
+        magnetic_variation = v[5]
+        point = gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=elevation, time=point_date_time, speed=current_speed)
+        point.magnetic_variation = magnetic_variation
+        gpx_segment.points.append(point)
 
     result = gpx.to_xml()
 
